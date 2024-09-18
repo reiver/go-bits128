d67f8bf95e9dc191de3661f21fa7fecbc9551433
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-09-18 15:19:31 -0700

    bits128.MaxUint128()

--- maxuint128.go
+++ maxuint128.go
@@ -2,15 +2,21 @@ package bits128
 
 func MaxUint128() Uint128 {
 
-	var ffffffffffffffffffffffffffffffff uint
+	// If uint is 64-bit, then this will end up being: 0xFFFFFFFFFFFFFFFF.
+	// If uint is 32-bit, then this will end up being: 0xFFFFFFFF.
+	//
+	// I.e., in 64-bit: 0x0000000000000000 - 1 = 0xFFFFFFFFFFFFFFFF
+	// I.e., in 32-bit: 0x00000000         - 1 = 0xFFFFFFFF
+	var ffffffffffffffff uint
 	{
-		ffffffffffffffffffffffffffffffff--
+		ffffffffffffffff--
 	}
 
 	var uint128 Uint128
-
-	for index:=0; index<SizeUints; index++ {
-		uint128.array[index] = ffffffffffffffffffffffffffffffff
+	{
+		for index:=0; index<SizeUints; index++ {
+			uint128.array[index] = ffffffffffffffff
+		}
 	}
 
 	return uint128
